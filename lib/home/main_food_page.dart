ebbdbc48559ed9c830d090541d78b5f49cc1884c
Benndora1 <Benndora2@gmail.com>
2022-12-26 18:25:47 +0300

    FoodPage

--- lib/home/main_food_page.dart
+++ lib/home/main_food_page.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:food_delivery/home/food_page_body.dart';
 import 'dart:ui';
+
+import '../utils/colors.dart';
+import '../widgets/big_text.dart';
+import '../widgets/small_text.dart';
 
 class MainFoodPage extends StatefulWidget {
   const MainFoodPage({Key? key}) : super(key: key);
@@ -19,26 +24,38 @@ class _MainFoodPageState extends State<MainFoodPage> {
       children: [
         Container(
           child: Container(
+            margin: EdgeInsets.only(top: 45, bottom: 15),
+            padding: EdgeInsets.only(left: 20, right: 20),
             child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Column(
                     children: [
-                      Text("County"),
-                      Text("Town"),
+                      BigText(text: "Kenya", color: AppColors.mainColor),
+                      Row(children: [
+                        SmallText(text: "Nairobi", color: Colors.black54),
+                        Icon(Icons.arrow_drop_down_rounded),
+                      ]),
                     ],
                   ),
-                  Container(
-                    width: 45,
-                    height: 45,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: Colors.blue,
+                  Center(
+                    child: Container(
+                      width: 45,
+                      height: 45,
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: AppColors.mainColor,
+                      ),
                     ),
                   )
                 ]),
           ),
         ),
+        FoodPageBody(),
       ],
     ));
   }
