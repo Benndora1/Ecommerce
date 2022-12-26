2db88826b8314dc6ae989460b9c6f48ea04bdceb
Benndora1 <Benndora2@gmail.com>
2022-12-26 22:39:45 +0300

    FoodPage

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -9,24 +9,44 @@ class FoodPageBody extends StatefulWidget {
 
 class _FoodPageBodyState extends State<FoodPageBody> {
   @override
+  PageController pageController = PageController(viewportFraction: 0.84);
   Widget build(BuildContext context) {
     return Container(
+        color: Colors.indigoAccent,
         height: 320,
         child: PageView.builder(
+            controller: pageController,
             itemCount: 5,
             itemBuilder: (context, position) {
               return _buildPageItem(position);
             }));
   }
 
-  Widget _buildPageItem(int position) {
-    return Container(
-      height: 220,
-      margin: EdgeInsets.only(left: 5, right: 5),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        color: Color(0xFF69C5DF),
-      ),
+  Widget _buildPageItem(int index) {
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: EdgeInsets.only(left: 5, right: 5),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: index.isEven ? Color(0xFF69C5DF) : Color(0xFF9294cc),
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/image/food0.png"))),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 140,
+            margin: EdgeInsets.only(left: 40, right: 40, bottom: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
