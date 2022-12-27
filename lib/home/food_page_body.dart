9284f4225371ee1b182d9c30d5bfa17105efb23b
Benndora1 <Benndora2@gmail.com>
2022-12-27 18:03:26 +0300

    FoodPage

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/icons_text_widgets.dart';
+
+import '../widgets/small_text.dart';
 
 class FoodPageBody extends StatefulWidget {
   const FoodPageBody({super.key});
@@ -10,9 +15,27 @@ class FoodPageBody extends StatefulWidget {
 class _FoodPageBodyState extends State<FoodPageBody> {
   @override
   PageController pageController = PageController(viewportFraction: 0.84);
+  var _currPageValue = 0.0;
+  double _scaleFactor = 0.8;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+      setState(() {
+        _currPageValue = pageController.page!;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Container(
-        color: Colors.indigoAccent,
+        // color: Colors.indigoAccent,
         height: 320,
         child: PageView.builder(
             controller: pageController,
@@ -23,30 +46,100 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Stack(
-      children: [
-        Container(
-          height: 220,
-          margin: EdgeInsets.only(left: 5, right: 5),
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: index.isEven ? Color(0xFF69C5DF) : Color(0xFF9294cc),
-              image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage("assets/image/food0.png"))),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            height: 140,
-            margin: EdgeInsets.only(left: 40, right: 40, bottom: 15),
+    // Scaling  code
+    Matrix4 matrix = new Matrix4.identity();
+    if (index == _currPageValue.floor()) {
+      var currScale = 1 - (_currPageValue - index) * (1 - _scaleFactor);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+    } else if (index == _currPageValue.floor() + 1) {
+      var currScale =
+          _scaleFactor + (_currPageValue - index) * (1 - _scaleFactor);
+      matrix = Matrix4.diagonal3Values(1, currScale, 1);
+    }
+    ;
+    return Transform(
+      transform: matrix,
+      child: Stack(
+        children: [
+          Container(
+            height: 220,
+            margin: EdgeInsets.only(left: 10, right: 10),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+                color: index.isEven ? Color(0xFF69C5DF) : Color(0xFF9294cc),
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage("assets/image/food0.png"))),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              height: 120,
+              margin: EdgeInsets.only(left: 30, right: 30, bottom: 30),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+              ),
+              child: Container(
+                padding: EdgeInsets.only(top: 15, right: 15, left: 15),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      BigText(text: "Kenyan Dishes"),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          Wrap(
+                            children: [
+                              Wrap(
+                                  children: List.generate(
+                                      5,
+                                      (index) => Icon(Icons.stars,
+                                          color: AppColors.mainColor,
+                                          size: 15))),
+                              SizedBox(width: 10),
+                              SmallText(text: "4.5"),
+                              SizedBox(width: 10),
+                              SmallText(text: "493"),
+                              SizedBox(width: 10),
+                              SmallText(text: "comments"),
+                            ],
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        children: [
+                          IconAndTextWidgets(
+                              icon: Icons.circle_sharp,
+                              text: "Normal",
+                              iconColor: AppColors.iconColor1),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          IconAndTextWidgets(
+                              icon: Icons.location_on,
+                              text: "17km",
+                              iconColor: AppColors.mainColor),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          IconAndTextWidgets(
+                              icon: Icons.access_time_rounded,
+                              text: "32mins",
+                              iconColor: AppColors.iconColor2)
+                        ],
+                      )
+                    ]),
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
---
+++ lib/widgets/icons_text_widgets.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:food_delivery/widgets/small_text.dart';
+
+class IconAndTextWidgets extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final Color iconColor;
+
+  const IconAndTextWidgets(
+      {super.key,
+      required this.icon,
+      required this.text,
+      required this.iconColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          icon,
+          color: iconColor,
+        ),
+        SizedBox(
+          width: 5,
+        ),
+        SmallText(
+          text: text,
+        )
+      ],
+    );
+  }
+}
